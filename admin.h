9b3a1f2fa60b9a23faadca0938815d330486f23e
Anshika Chhabra <124261062+anshika1812@users.noreply.github.com>
2023-11-14 09:33:01 +0530

    admin.h

--- admin.h
+++ admin.h
@@ -9,9 +9,9 @@ int adminmenu(){
 	int n=0;
     do{
         adminheader();
-        printf("\t\t\tPress '1' to view list of Books\n");
-        printf("\t\t\tPress '2' to add a new Books in the list\n");
-        printf("\t\t\tPress '3' to delete a Book from the list\n");
+        printf("\t\t\tPress '1' to view list of Books in the Library\n");
+        printf("\t\t\tPress '2' to add new Books in the Library\n");
+        printf("\t\t\tPress '3' to delete a Book from the Library\n");
         printf("\t\t\tPress '4' to check Book status\n");
         printf("\t\t\tPress '0' to return to main menu\n");
         scanf("\t\t\t%d",&n);
@@ -37,7 +37,38 @@ int adminmenu(){
 	return 0;
 }
 
+struct node{
+	char bookname[150];
+    int qty;
+    struct node*loc;
+};
+struct node*start=NULL;
+
 int addBooks(){
+    int num,i,qty;
+    char name[100],ch[100],fname;
+    struct node*temp=(struct node*)malloc(sizeof(struct node));
+    struct node*t=NULL;
+    start=temp;
+    printf("\t\t\tHow many Books do you wish to add? ");
+    scanf("%d",&num);
+    for(i=0;i<num;i++){
+        printf("\t\t\tEnter name of Book: ");
+        scanf("%s",name);
+        printf("\t\t\tEnter quantity: ");
+        scanf("%d",&qty);
+        strcpy(temp->bookname,name);
+    	temp->qty=qty;
+    	FILE*p=fopen("books.txt","a");
+    	fprintf(p,"%s %d\n",name,qty);
+    	fclose(p);
+        temp->loc=(struct node*)malloc(sizeof(struct node));
+        t=temp;
+        temp=temp->loc;
+    }
+    free(temp);
+    t->loc=NULL;
+    printf("\t\t\tAdded successfully\n\n");
 	return 0;
 }
 
