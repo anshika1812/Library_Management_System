11c12e1dc476e8695e1d58f8ebe987296f3e9fde
Anshika Chhabra <124261062+anshika1812@users.noreply.github.com>
2023-12-28 14:03:47 +0530

    admin.h

--- admin.h
+++ admin.h
@@ -13,6 +13,8 @@ int adminmenu(){
         printf("\t\t\tPress '2' to add new Books in the Library\n");
         printf("\t\t\tPress '3' to delete a Book from the Library\n");
         printf("\t\t\tPress '4' to check Book status\n");
+        printf("\t\t\tPress '5' to issue a Book to a user\n");
+        printf("\t\t\tPress '6' to process return from a user\n");
         printf("\t\t\tPress '0' to return to main menu\n");
         scanf("\t\t\t%d",&n);
         if(n==0){
@@ -30,6 +32,12 @@ int adminmenu(){
         else if(n==4){
         	checkStatus();
 		}
+		else if(n==5){
+        	issueBook();
+		}
+		else if(n==4){
+        	returnBook();
+		}
         else{
             printf("\t\t\tInvalid choice entered.");
         }
@@ -40,16 +48,77 @@ int adminmenu(){
 struct node{
 	char bookname[150];
     int qty;
+    int issued;
     struct node*loc;
 };
 struct node*start=NULL;
 
+int updateQty(char name[],int newqty){
+	//reading data from file into linked list so as to make updation of quantity possible
+    struct node*bookStart=NULL;
+    struct node*bookEnd=NULL;
+    FILE*p=fopen("books.txt","r");
+    if(p==NULL){
+    	addBooks();
+    	return 0;
+	}
+	char bookData[200];
+    while(fgets(bookData,sizeof(bookData),p)!=NULL){
+    	char exname[100];
+    	int exqty,exissued;
+        if(sscanf(bookData,"%s %d %d",exname,&exqty,&exissued)==3){
+        	struct node*temp=(struct node*)malloc(sizeof(struct node));
+        	strcpy(temp->bookname,exname);
+        	temp->qty=exqty;
+        	temp->issued=exissued;
+        	temp->loc=NULL;
+        	
+        	if(bookEnd==NULL){
+        		bookStart=temp;
+			}
+			else{
+				bookEnd->loc=temp;
+			}
+			bookEnd=temp;
+		}
+	}
+	fclose(p);
+	int bookPresent=0;
+	struct node*current=bookStart;
+    while(current!=NULL) {
+        if(strcmp(current->bookname,name)==0) {
+            //book found, update the quantity
+            bookPresent=1;
+            break;
+        }
+        current=current->loc;
+    }
+    if(bookPresent==0){
+    	return -1;
+	}
+	else{
+		//update the file by overwriting
+		p=fopen("books.txt","w");
+		current=bookStart;
+    	while(current!=NULL){
+	    	if(strcmp(current->bookname,name)==0) {
+	            current->qty+=newqty;
+	        }
+	        fprintf(p,"%s %d %d\n",current->bookname,current->qty,current->issued);
+	        struct node*temp=current;
+	        current=current->loc;
+    	}
+		fclose(p);
+		return 0;
+	}
+}
+
 int addBooks(){
-    int num,i,qty;
+    int num,i,qty,issued,cont;
     char name[100],ch[100],fname;
-    struct node*temp=(struct node*)malloc(sizeof(struct node));
+    struct node*t1=(struct node*)malloc(sizeof(struct node));
     struct node*t=NULL;
-    start=temp;
+    start=t1;
     printf("\t\t\tHow many Books do you wish to add? ");
     scanf("%d",&num);
     for(i=0;i<num;i++){
@@ -57,23 +126,45 @@ int addBooks(){
         scanf("%s",name);
         printf("\t\t\tEnter quantity: ");
         scanf("%d",&qty);
-        strcpy(temp->bookname,name);
-    	temp->qty=qty;
-    	FILE*p=fopen("books.txt","a");
-    	fprintf(p,"%s %d\n",name,qty);
-    	fclose(p);
-        temp->loc=(struct node*)malloc(sizeof(struct node));
-        t=temp;
-        temp=temp->loc;
+        cont=updateQty(name,qty);
+        if(cont==-1){
+	        strcpy(t1->bookname,name);
+	    	t1->qty=qty;
+	    	issued=0;
+	    	t1->issued=issued;
+	    	FILE*p=fopen("books.txt","a");
+	    	fprintf(p,"%s %d %d\n",name,qty,issued);
+	    	fclose(p);
+	        t1->loc=(struct node*)malloc(sizeof(struct node));
+	        t=t1;
+	        t1=t1->loc;
+		}
     }
-    free(temp);
-    t->loc=NULL;
+    free(t1);
+    if(t!=NULL){
+    	t->loc=NULL;
+	}
     printf("\t\t\tAdded successfully\n\n");
 	return 0;
 }
 
 int displayBooks(){
-	return 0;
+	char booknqty[200];
+	char name[100];
+    int qty,issued;
+	FILE*p=fopen("books.txt","r");
+	if(p==NULL){
+		printf("Error displaying...");
+		return 0;
+	}
+	printf("\n\t\t\tBOOK NAME\t\t\t\tQUANTITY\n\n",name,qty);
+    while(fgets(booknqty,sizeof(booknqty),p)!=NULL){
+        if(sscanf(booknqty,"%s %d %d",name,&qty,&issued)==3) {
+            printf("\t\t\t%s\t\t\t\t%d\n",name,qty,issued);
+        }
+    }
+    fclose(p);
+    return 0;
 }
 
 int deleteBook(){
@@ -81,5 +172,13 @@ int deleteBook(){
 }
 
 int checkStatus(){
+	return 0;
+}
+
+int issueBook(){
+	return 0;
+}
+
+int returnBook(){
 	return 0;
 }
